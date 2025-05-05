53d20d0d69b3b3ca08deb2215a2cc97ece622cc6
jared <jaredrotich3@gmail.com>
2025-05-05 12:21:41 +0300

    added social links

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,11 +1,23 @@
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi"
 
-export default function Contact() {
-    return (
-      <section>
-        <h1>Contact Me</h1>
-        <p>Email: jaredrotich3@gmail.com</p>
-        
-      </section>
-    );
-  }
-  
+function SocialLinks() {
+  return (
+    <div className="social-links">
+      <a href="https://github.com/jaredrotich " target="_blank"  rel="noopener noreferrer">
+        <FaGithub size={30} />
+      </a>
+      <a href="https://www.linkedin.com/feed/ " target="_blank" rel="noopener noreferrer">
+        <FaLinkedin size={30} />
+      </a>
+      <a href="jaredrotich3@gmail.com">
+        <HiOutlineMail size={30} />
+      </a>
+      <a href="https://x.com/JaredRotich12" target="_blank" rel="noopener noreferrer">
+        <FaTwitter size={30} />
+      </a>
+    </div>
+  );
+}
+
+export default SocialLinks;
